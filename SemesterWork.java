e7cb941139781ab8ecf0119c5d6b0e81a69663f1
Muhammad Ahsan <mahsansiddiqui2@gmail.com>
2023-02-08 16:26:46 +0500

    started project

---
+++ SemesterWork.java
@@ -0,0 +1,29 @@
+/*
+
+Dataset: https://www.kaggle.com/datasets/syedasimalishah/auto-
+pedestrians-crashes
+
+Step 1: Make a queue for the car crashes based on their time line from January to December (Day 1 to
+Day 31) and store day of week, crash type, light conditions, weather conditions and other necessary data.
+
+Step 2: Make three sub queues from the original queue such as sub queues storing only clear weather
+crashes, snow weather crashes and rain weather crashes.
+
+Step 3: Make seven sub queues (one for each day of the week) for all crashes.
+
+Step 4: Make two binary trees to store total number of crashes for each month(use crash number as key)
+for both ‘hit and run’ and ‘not hit and run’ crash types.
+
+
+Problem 1: How to find out which day (from Monday to Sunday) was the deadliest of all days as well as
+which months’ Friday was the deadliest among all Fridays.
+
+Problem 2: How to find the number of rainy day crashes in every month.
+
+Problem 3: Which month had the lowest ‘hit and run’ and which month had the highest ‘hit and run’ crash
+cases (use binary tree already developed). Which month has lowest ‘not hit and run’ and which month had
+the highest ‘not hit and run’ crash cases
+
+Postulate 1: Is it true that the weekends (Saturdays and Sundays) when the light conditions were dark,
+caused more crashes than Mondays and Tuesdays.
+*/
